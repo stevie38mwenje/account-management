7169e1893e88cc3e0de5e92953dba99184f60305
stevemwenje <stephen.mwenje@equitybank.co.ke>
2022-02-17 12:14:36 +0300

    accounts

--- src/main/java/com/example/accounts/service/impl/AccountServiceImpl.java
+++ src/main/java/com/example/accounts/service/impl/AccountServiceImpl.java
@@ -1,7 +1,9 @@
 package com.example.accounts.service.impl;
 
 import com.example.accounts.model.Account;
+import com.example.accounts.repo.AccountRepo;
 import com.example.accounts.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,13 +11,17 @@ import java.util.List;
 
 @Service
 public class AccountServiceImpl implements AccountService {
+
+    @Autowired
+    private AccountRepo accountRepo;
+
     @Override
     public Account insertAccount(Account accountPayload) {
-        return null;
+        return accountRepo.save(accountPayload);
     }
 
     @Override
     public List<Account> findAllAccounts() {
-        return null;
+        return accountRepo.findAll();
     }
 }
